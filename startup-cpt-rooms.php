19054ed606ac9453726e78b009f361a4a0537bde
yozz <yozz@Manava.local>
2015-08-18 21:53:48 -0400

    GitHub Plugin Updater

--- startup-cpt-rooms.php
+++ startup-cpt-rooms.php
@@ -1,10 +1,34 @@
 <?php
 /*
-Plugin Name: StartUp Rooms Custom Post Type
+Plugin Name: StartUp Rooms
 Description: Le plugin pour activer le Custom Post Rooms
 Author: Yann Caplain
-Version: 0.2.0
+Version: 0.3.0
 */
+
+//GitHub Plugin Updater
+function startup_reloaded_rooms_updater() {
+	include_once 'lib/updater.php';
+	define( 'WP_GITHUB_FORCE_UPDATE', true );
+	if ( is_admin() ) {
+		$config = array(
+			'slug' => plugin_basename( __FILE__ ),
+			'proper_folder_name' => 'startup-cpt-rooms',
+			'api_url' => 'https://api.github.com/repos/yozzi/startup-cpt-rooms',
+			'raw_url' => 'https://raw.github.com/yozzi/startup-cpt-rooms/master',
+			'github_url' => 'https://github.com/yozzi/startup-cpt-rooms',
+			'zip_url' => 'https://github.com/yozzi/startup-cpt-rooms/archive/master.zip',
+			'sslverify' => true,
+			'requires' => '3.0',
+			'tested' => '3.3',
+			'readme' => 'README.md',
+			'access_token' => '',
+		);
+		new WP_GitHub_Updater( $config );
+	}
+}
+
+add_action( 'init', 'startup_reloaded_rooms_updater' );
 
 //CPT
 function startup_reloaded_rooms() {
